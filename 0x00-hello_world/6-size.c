ed247df0009366de7053a536828109a5570c0427
elhalimahairat <maha.hali2002@gmail.com>
2023-03-16 19:19:00 -0700

    hoho

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  /* main good */
+int i ;
+float l ;
+char z ;
+printf("Size of a char: %lu byte(s)", sizeof(z));
+printf("Size of an int: %lu byte(s)", sizeof(i);
+printf("Size of a long int: %lu byte(s)",(usigned long)sizeof(i));
+printf("Size of a long long int: %lu byte(s)", (usigned long long)sizeof(i));
+printf("Size of a float: %lu byte(s)", sizeof(l));
+
+return (0);
+}
