ae65e040fd8cabfc6130414e91a7b6e6bc10a8f5
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-18 00:30:55 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,33 +6,13 @@
  */
 int main(void)
 {
-char irat[] = "abcdefghijklmnopqrstuvwxyz"; 
-putchar(irat[0]);
-putchar(irat[1]);
-putchar(irat[2]);
-putchar(irat[3]);
-putchar(irat[4]);
-putchar(irat[5]);
-putchar(irat[6]);
-putchar(irat[7]);
-putchar(irat[8]);
-putchar(irat[9]);
-putchar(irat[10]);
-putchar(irat[11]);
-putchar(irat[12]);
-putchar(irat[13]);
-putchar(irat[14]);
-putchar(irat[15]);
-putchar(irat[16]);
-putchar(irat[17]);
-putchar(irat[18]);
-putchar(irat[19]);
-putchar(irat[20]);
-putchar(irat[21]);
-putchar(irat[22]);
-putchar(irat[23]);
-putchar(irat[24]);
-putchar(irat[25]);
+int x = 'A';
+while (x <= 'Z') {
+x = tolower(x);
+putchar(x);
+x ++;
+
+}
 putchar('\n');
 return (0);
 }
