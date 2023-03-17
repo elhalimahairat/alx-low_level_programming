f7a8dac5c937053c6354641934cc954a02c63e70
elhalimahairat <maha.hali2002@gmail.com>
2023-03-17 16:58:53 -0700

    gdsd

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+char x[] = "abcdefghijklmnopqrstuvwxyz";
+while (i < 26)
+{
+putchar(x[i]);
+i++;
+
+}
+putchar('\n');
+return (0);
+}
