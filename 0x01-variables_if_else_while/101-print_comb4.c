ac25cba4cea0b38262633076e1e6dd6c45f4ab4d
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-18 01:37:15 +0100

    Update and rename 6-print_numberz.c to 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char h = '0', t = '1', u = '2';
+while ((h <= '7') || (t <= '8') || (u <= '9')) {
+printf ("%c%c%c, ", h, t, u);
+u != '9' ? ++u : (t != '8' ? (++t, u = t + 1) : (++h, t = h + 1, u = t + 1));
+}
+putchar('\n');
+return (0);
+}
