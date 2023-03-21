10b636f23db853ce90787e2af68765a5c6856514
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-22 00:30:02 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,15 @@
-#include"irat.h"
+#include<stdio.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+void print_alphabet(void);
 int main(void)
 {
 print_alphabet();
 return (0);
+}
+void print_alphabet(void){
+printf("abcdefghijklmnopqrstuvwxyz\n");
 }
