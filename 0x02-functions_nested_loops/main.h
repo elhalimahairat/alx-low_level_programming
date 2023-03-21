643228e4529c2c5d7e703f67582309c4e383a5d4
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-21 23:03:27 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,11 @@
 void print__putchar();
+void print_alphabet(void);
+
 void print__putchar(){
 printf("_putchar\n");
 return;
 }
-void print_alphabet(void);
+
 void print_alphabet(void){
 printf("abcdefghijklmnopqrstuvwxyz\n");
 return;
