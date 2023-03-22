66edaed2e9b4af9224a4c1d1a0c01c980d0e9749
elhalimahairat <maha.hali2002@gmail.com>
2023-03-21 18:07:36 -0700

    s

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void Fibonacci_1(void);
+int main(void)
+{
+Fibonacci_1();
+return (0);
+}
+/**
+ * Fibonacci_1 - check the code.
+ *
+ * end in 50.
+ */
+void Fibonacci_1(void)
+{
+long sum = 0;
+long i = 1;
+long j = 2;
+long l = 4000000;
+long m = i + j;
+do {
+if ((i % 2) == 0)
+sum += i;
+if ((j % 2) == 0)
+sum += j;
+if ((m % 2) == 0)
+sum += m;
+i = j + m;
+j = m + i;
+m = i + j;
+} while ((i < l) || (j < l) || (m < l));
+printf("%ld\n", sum);
+return;
+}
