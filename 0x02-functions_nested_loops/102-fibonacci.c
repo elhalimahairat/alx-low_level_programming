68d7a9aecb40352efbcbfa7e9f0543d88e2b3878
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-22 01:18:56 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -16,11 +16,14 @@ long j = 2;
 long m= 3;
 long h =  1;
 long l = 32951280099;
+long k = 1258626;
 do{
 printf("%ld, ", i);
 printf("%ld, ", j);
-if(m < l )
+if((m < l) && (m != k) )
 printf("%ld, ", m);
+else if(m == k)
+printf("%ld", m);
 i = j + m;
 j = m + i;
 m = i + j;
