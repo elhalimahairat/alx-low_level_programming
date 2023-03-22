acf723650fc329f0d837755abbe02f1e5b906259
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-22 01:26:38 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,31 +4,32 @@
  *
  * Return: Always 0.
  */
-void Fibonacci();
+void Fibonacci(void);
 int main(void)
 {
 Fibonacci();
 return (0);
 }
-void Fibonacci(void){
+void Fibonacci(void)
+{
 long i = 1;
 long j = 2;
-long m= 3;
+long m = 3;
 long h =  1;
 long l = 32951280099;
 long k = 20365011074;
 do{
-printf("%ld, ", i);
-if(j != k)
-printf("%ld, ", j);
+printf ("%ld, ", i);
+if (j != k)
+printf ("%ld, ", j);
 else
-printf("%ld", j);
-if(m < l )
-printf("%ld, ", m);
+printf ("%ld", j);
+if (m < l )
+printf ("%ld, ", m);
 i = j + m;
 j = m + i;
 m = i + j;
-h =  20365011074;
+h = 20365011074;
 }while(i <= h);
 printf("\n");
 return;
