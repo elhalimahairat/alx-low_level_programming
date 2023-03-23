17db910c03b322b50a5c1e030ab12011d5e4605c
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-24 00:19:16 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,4 +8,14 @@ return (1);
 else
 return (0);
 }
+void _putchar(void);
+void _putchar(void){
+   char c;
+
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return;
+}
 #endif
