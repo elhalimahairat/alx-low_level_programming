628f15ba3bafb795fcf9355af74b3e2d3d4be614
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-24 00:35:56 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,17 @@
 #ifndef main
 #define main
 int _isupper(int c);
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
-if ((c >= 65) && (c <= 90))
-return (1);
-else
-return (0);
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 void _putchar(void);
 void _putchar(void){
