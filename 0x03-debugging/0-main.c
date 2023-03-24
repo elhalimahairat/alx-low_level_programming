38e15ebe96732598362ab1b76e86ca72cdb508f9
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-24 02:25:34 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,13 @@
 * Return: 0
 */
 
-int main(void)
+void positive_or_negative(int i)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i < 0)
+printf("%d is negative\n", i);
+else if (i == 0)
+printf("%d is zero\n", i);
+return;
 }
