c1b07d1ab5a89e7b1488c3ca70ce1a3355e246ae
elhalimahairat <maha.hali2002@gmail.com>
2023-03-24 16:46:03 -0700

    fd

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+int i = 0;
+while (i < n)
+{	
+_putchar('_');
+i++;
+}
+_putchar('\n');
+return;
+}
