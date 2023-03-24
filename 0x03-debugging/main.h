9eba93236d88b067b630352da69ae22a92a523cc
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-24 02:20:30 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,26 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-void positive_or_negative(int i);
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-void positive_or_negative(int i)
-{
-	/* your code goes there */
-if (i > 0)
-printf("%d is positive\n", n);
-else if (i < 0)
-printf("%d is negative\n", n);
-else if (i == 0)
-printf("%d is zero\n", n);
-	return;
-}
+void positive_or_negative(int i);
+
 #endif /* MAIN_H */
