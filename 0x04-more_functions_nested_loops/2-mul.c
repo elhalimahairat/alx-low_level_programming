665ea3cbcf4aeee95c560206020fcbf8d6e07186
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-24 23:39:05 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * mul
- * mul == a*b
- * int b == ?
- * int a == ?
+ * mul == a b
+ * int b == int a == 
  */
 int mul(int a, int b)
 {
 /**
- * int b == ?
- * int a == ?
+ * int b == 
+ * int a == 
  * mul x 
  */
 int mul;
