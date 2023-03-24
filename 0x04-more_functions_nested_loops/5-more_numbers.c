0322f19818767a99ff818d7070159e034a4ad52a
elhalimahairat <maha.hali2002@gmail.com>
2023-03-24 16:23:01 -0700

    sf

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int i = 0;
+int j;
+while (i < 10)
+{
+j = 0;
+while (j < 15)
+{
+printf("%d", i);
+j++;
+}
+i++;
+}
+printf("\n");
+return;
+}
