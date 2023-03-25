37d13e2bc83773fd25d92979056add206227b066
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-25 15:24:15 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,35 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int _putchar(char c);
-/**
- * main - hgf
- *
- * Return: Aldfgh
- */
 int _isupper(int c);
-/**
- * main asdfg
- *
- *sdfg
- */
 int _isdigit(int c);
-/**
- * main - Entrygsdsgf
- *
- * Return: Always 0 (Setr
- */
 int mul(int a, int b);
-/**
- * maiddfdf
- *
- * Return: Always 0 (Success)
- */
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -37,4 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 #endif /*MAIN_H*/
