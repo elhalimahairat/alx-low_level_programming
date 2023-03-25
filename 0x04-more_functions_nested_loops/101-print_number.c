5dd18b170c106adcf164654b900864e9e93fd87b
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-25 15:56:29 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,65 @@
 void print_number(int n)
 {
 if (n >=)
+if (n < 10)
 _putchar(48 + n);
+else
+{
+if (n >= 1000000000)
+int i = 10000000000 ;
+while (i >= 1){
+_putchar(48 + n % i);
+i /= 10;
+}
+else if ()
+{
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+}
+else if ()
+{
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+}
+else if ()
+{
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+}
+else if ()
+{
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+_putchar(48 + );
+}
+}
 else
 n = -n;
 _putchar('-');
