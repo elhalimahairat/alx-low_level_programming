fe1e8a2b6eef86efacfad443a61d626b89b7b077
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-25 02:59:42 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -13,18 +13,3 @@ int main(void)
 
         return (0);
 }
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-void positive_or_negative(int i)
-{
-if (i > 0)
-printf("%d is positive\n", i);
-else if (i < 0)
-printf("%d is negative\n", i);
-else if (i == 0)
-printf("%d is zero\n", i);
-return;
-}
