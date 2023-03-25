887aa397f58232d6a51724092f69d60315261c6b
elhalimahairat <125500348+elhalimahairat@users.noreply.github.com>
2023-03-25 15:58:17 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include<stdio.h>
+
 void print__putchar();
+
 void print_alphabet(void);
 
-void print__putchar(){
-printf("_putchar\n");
-return;
-}
-void print_alphabet(void){
-printf("abcdefghijklmnopqrstuvwxyz\n");
-return;
-}
 #endif /*MAIN_H*/
