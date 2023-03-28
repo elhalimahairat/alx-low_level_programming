7eecc1805115e7b1b10ce72fa7691b37f5f9beae
elhalimahairat <maha.hali2002@gmail.com>
2023-03-27 18:52:32 -0700

    df

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_rev -a fonction print all of array in rev
+ *
+ *@s : array
+ *
+ *Return: all array
+ */
+void print_rev(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+i++;
+while ([i] != 0)
+{
+i--;
+printf("%c", s[i]);
+}
+printf("\n");
+return;
+}
