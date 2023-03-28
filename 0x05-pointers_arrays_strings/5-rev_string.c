911e1af52a595ef235950177399641f6077e1f57
elhalimahairat <maha.hali2002@gmail.com>
2023-03-27 19:05:13 -0700

    sd

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *rev_string -a fonction print all of array in rev
+ *
+ *@s : array
+ *
+ *Return: all array
+ */
+void rev_string(char *s)
+{
+int i, n;
+i = 0;
+n = 0;
+while (s[i] != '\0')
+i++;
+while (i != 0)
+{
+char m;
+m = s[n];
+i--;
+s[i] = s[n];
+s[n] = m;
+n++;
+}
+return;
+}
