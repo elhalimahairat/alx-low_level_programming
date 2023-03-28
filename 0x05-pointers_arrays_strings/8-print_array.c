de73ccda273781a2ea8f64c7f52878537b6099c1
elhalimahairat <maha.hali2002@gmail.com>
2023-03-28 16:11:56 -0700

    irat boss

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: array to be printed
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n - 1; i++)
+printf("%d, ", a[i]);
+printf("%d\n", a[i]);
+return;
+}
