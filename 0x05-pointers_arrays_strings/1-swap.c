4afebe98d47a5240bc45b393bd6c91aefe235ffd
elhalimahairat <maha.hali2002@gmail.com>
2023-03-27 18:14:49 -0700

    s

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *swap_int -a fonction swap two int 
+ *swap_int -a fonction swap two int
  *
  *@a : pointer
  *
  *@b : pointer
  *
- *return : a and b 
+ *return : a and b
  */
 void swap_int(int *a, int *b)
 {
