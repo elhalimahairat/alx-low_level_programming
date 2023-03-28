d35570aa84cd83fee607dd4b280b5e315cae0b2c
elhalimahairat <maha.hali2002@gmail.com>
2023-03-27 18:24:42 -0700

    sd

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strlen -a fonction print size of array
+ *
+ *@s : array
+ *
+ * return : size of array
+ */
+int _strlen(char *s)
+{
+int i, size;
+i = 0;
+size = 0;
+while (s[i] != 0/)
+{
+i++;
+size++;
+}
+return (size);
+}
