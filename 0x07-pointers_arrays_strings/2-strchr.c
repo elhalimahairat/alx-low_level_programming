322ec6f25783576d17e08dd1dc34bbf7e26ba2c6
elhalimahairat <maha.hali2002@gmail.com>
2023-04-04 16:46:49 -0700

    asf

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,15 @@ char *_strchr(char *s, char c)
 {
 int a;
 while (1)
-   {
-     a = *s++;
-     if (a == c)
-     {
-	     return (s - 1);
-     }
-     if (a == 0)
-     {
-	     return (NULL);
-     }
-   }
+{
+a = *s++;
+if (a == c)
+{
+return (s - 1);
+}
+if (a == 0)
+{
+return (NULL);
+}
+}
 }
